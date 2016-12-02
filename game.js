b6255b003fa8d908d45a523693ca82e10719225a
Charles Dowell <iggydowell@gmail.com>
2016-12-01 19:32:00 -0800

    wwwwep

--- game.js
+++ game.js
@@ -22,13 +22,31 @@ document.addEventListener("keydown", function(e) {
   var FoodX = Number(document.getElementById("spag").getAttribute("x"));
   var FoodY = Number(document.getElementById("spag").getAttribute("y"));
 
+  var EneX = Number(document.getElementById("frot").getAttribute("x"));
+  var EneY = Number(document.getElementById("frot").getAttribute("y"));
+
   var Food2X = Number(document.getElementById("spago").getAttribute("x"));
   var Food2Y = Number(document.getElementById("spago").getAttribute("y"));
 
   var ran = Math.floor((Math.random() * 650) + 0);
   var ran1 = Math.floor((Math.random() * 650) + 0);
 
-
+  if(PlayX >= EneX - 100 && PlayX <= EneX + 100 && PlayY >= EneY - 100 && PlayY <= EneY + 100) {
+    tx.innerHTML = "You Lost!";
+    endcheck = 1;
+    tx.setAttribute("opacity", 1);
+    var e = document.getElementById("end");
+    e.setAttribute("opacity", 1);
+    var y = document.getElementById("chro");
+    y.setAttribute("opacity", 0);
+    wop.setAttribute("opacity", 1)
+        document.getElementById("p").pauseAnimations()
+    //randur1*=0.9;
+    //document.getElementById("spagA").setAttribute("dur", randur1);
+    //foodEaten++;
+    //document.getElementById("chro").textContent = "Chromosomes Collected: ";
+    //    document.getElementById("chro").textContent += foodEaten;
+  }
 
 
   if(PlayX >= FoodX - 100 && PlayX <= FoodX + 100 && PlayY >= FoodY - 100 && PlayY <= FoodY + 100) {
@@ -61,6 +79,7 @@ var y = document.getElementById("chro");
 y.setAttribute("opacity", 0);
 wop.setAttribute("opacity", 1)
     document.getElementById("p").pauseAnimations()
+
 }
 
 
@@ -101,5 +120,10 @@ wop.setAttribute("opacity", 1)
   b.setAttribute("y", yval);
 
   }
+
 }
 )
+
+setInterval(
+
+  function(){ if(bool == true){ var sol = Math.floor((Math.random() * 650) + 0); document.getElementById("frot").setAttribute("y",sol)}},7000);
