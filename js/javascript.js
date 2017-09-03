cd45aeb2e329caaf9e36be330357a9c84d77cfb7
Empoleo <iggydowell@gmail.com>
2017-09-02 21:40:12 -0700

    update
    wow this took years

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+function music() {
+$("#container").load("html/music.html .music");
+}
+
+function home() {
+$("#container").load("html/home.html .home");
+}
+
+function about() {
+$("#container").load("html/about.html .about");
+}
+
+function merchandise() {
+$("#container").load("html/merchandise.html .merchandise");
+}
+
+function tour() {
+$("#container").load("html/tour.html .tour");
+}
+
