1d9f55d471a0e4b7488198cd0b8f719eb3a1fa6a
Empoleo <iggydowell@gmail.com>
2017-09-04 01:54:37 -0700

    1
    2

--- js/javascript.js
+++ js/javascript.js
@@ -18,3 +18,60 @@ function tour() {
 $("#container").load("html/tour.html .tour");
 }
 
+var hurtval = false;
+var insideval = false;
+var melodyval = false;
+var blindval = false;
+
+function hurtlist(){
+if(hurtval==false){
+
+}
+else{
+hurtval=true;
+}
+}
+
+function insidelist(){
+if(insideval==false){
+    insideval=true;
+document.getElementById("insidelist").innerHTML="<ul class='list-group'><li class='list-group-item'><b>Songs:</b></li><li class='list-group-item'>heartbeat - 1.99$</li><li class='list-group-item'>disconnect - 1.99$</li><li class='list-group-item'>corrosion - 1.99$</li><li class='list-group-item'>loop - 1.99$</li></ul>"
+}
+else{
+insideval=false;
+document.getElementById("insidelist").innerHTML=""
+}
+}
+
+function hurtlist(){
+if(hurtval==false){
+    hurtval=true;
+document.getElementById("hurtlist").innerHTML="<ul class='list-group'><li class='list-group-item'><b>Songs:</b></li><li class='list-group-item'>missing - 1.99$</li><li class='list-group-item'>blood - 1.99$</li><li class='list-group-item'>break - 1.99$</li><li class='list-group-item'>forgotten - 1.99$</li></ul>"
+}
+else{
+hurtval=false;
+document.getElementById("hurtlist").innerHTML=""
+}
+}
+
+function blindlist(){
+if(blindval==false){
+    blindval=true;
+document.getElementById("blindlist").innerHTML="<ul class='list-group'><li class='list-group-item'><b>Songs:</b></li><li class='list-group-item'>ignorant - 1.99$</li><li class='list-group-item'>past - 1.99$</li><li class='list-group-item'>return - 1.99$</li><li class='list-group-item'>block - 1.99$</li></ul>"
+}
+else{
+blindval=false;
+document.getElementById("blindlist").innerHTML=""
+}
+}
+
+function melodylist(){
+if(melodyval==false){
+    melodyval=true;
+document.getElementById("melodylist").innerHTML="<ul class='list-group'><li class='list-group-item'><b>Songs:</b></li><li class='list-group-item'>tuned - 1.99$</li><li class='list-group-item'>focus - 1.99$</li><li class='list-group-item'>note - 1.99$</li><li class='list-group-item'>gone - 1.99$</li></ul>"
+}
+else{
+melodyval=false;
+document.getElementById("melodylist").innerHTML=""
+}
+}
